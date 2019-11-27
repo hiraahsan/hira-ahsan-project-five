98729e264ab07c124655df7dbacf8c861a8505a8
hira <hiraahsan94@gmail.com>
2019-11-27 16:20:24 -0500

    initial commit:party:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import firebase from './firebase';
+// import ImageSection from './ImageSection';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Writer's Block</h1>
+        </header>
+        {/* Insert instructions here
+        pop up?
+        or <details> */}
+        {/* Create component for image sections */}
+      </div>
+    );
+
+  }
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAyf12FdGTgypQ0Ae48KszC61cViIsSRAU",
+    authDomain: "project-five-tests.firebaseapp.com",
+    databaseURL: "https://project-five-tests.firebaseio.com",
+    projectId: "project-five-tests",
+    storageBucket: "project-five-tests.appspot.com",
+    messagingSenderId: "909191868794",
+    appId: "1:909191868794:web:1046029a4765b1b69566c3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
