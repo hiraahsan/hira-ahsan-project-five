dff369e05584af8774452148cab867df82edcb12
hira <hiraahsan94@gmail.com>
2019-11-30 16:11:27 -0500

    added functionality for storing in firebase and loading on page load

--- src/App.js
+++ src/App.js
@@ -13,31 +13,21 @@ class App extends Component {
     }
   }
 
-  // handleChange = (event) => {
-  //   event.preventDefault();
-  //   console.log('event', event.target)
-
-  //   // const node = document.createElement('div')
-  //   // const textnode = document.createTextNode(this.state.mappedArray)
-  //   for (let i = 0; i < this.state.mappedArray.length; i++) {
-  //     appendHere.appendChild(`<img src=${this.state.mappedArray[i]} />`)
-  //   }
-
-    // console.log('i worked!', this.state.resultArray)
-  // }
-
   render() {
-
-    // console.log(this.state.mappedArray);
 
     return (
       <div className="App">
         <header>
           <h1>Writer's Block</h1>
         </header>
+
+        <details>
+          <summary>
+            <p>Testing the details tab</p>
+            <p>Testing again</p>
+          </summary>
+        </details>
         <ImageSection />
-        {/* {this.displayData} */}
-        {/* {this.images()} */}
         {/* Insert instructions here
         as pop up?
         or <details> */}
--- src/ImageSection.js
+++ src/ImageSection.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import firebase from 'firebase/app';
 import Images from './Images';
+import StoredImages from './StoredImages';
 import StorySection from './StorySection';
 
+// To-Do: filter the selections by their height/widths?? and image types
 
 class ImageSection extends Component {
 
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
             apiKey: '14421506-770fc3d3a51ab16bab09705a9',
             apiUrl: 'https://pixabay.com/api/',
@@ -16,9 +18,13 @@ class ImageSection extends Component {
             data: [],
             mappedArray: [],
             imageArray: [],
+            textArray: [],
             imageToAppend: '',
             userInput: '',
-            dbRef: firebase.database().ref(),
+            mappedDbref: '',
+            isButtonDisabled: false,
+            dbRefImages: firebase.database().ref('/images'),
+            dbRefText: firebase.database().ref('/text')
         }
     }
 
@@ -34,13 +40,45 @@ class ImageSection extends Component {
                     })
                 })
             }) 
+        
+        this.state.dbRefImages.on('value', (snapshot) => {
+            // console.log(snapshot.val());
+            const images = snapshot.val();
+            const newImages = []
 
-        // To-Do: connect app to firebase
+            for (let key in images) {
+                const individualImages = {
+                    imageId: key,
+                    imageUrl: images[key]
+                }
+                newImages.push(individualImages);
+                // console.log(newImages);
+            }
 
+            this.setState({
+                imageArray: newImages
+            })
+        })
 
+        this.state.dbRefText.on('value', (snapshot) => {
+            console.log(snapshot.val());
+            const stories = snapshot.val();
+            const newStories = []
+
+            for (let key in stories) {
+                const individualStories = {
+                    storyId: key,
+                    storyText: stories[key]
+                }
+                newStories.push(individualStories);
+                // console.log(newStories);
+            }
+
+            this.setState({
+                textArray: newStories
+            })
+        })
     }
-
-    
 
     handleChange = () => {
         console.log(document.querySelector('input[name="radio"]:checked').value)
@@ -50,12 +88,22 @@ class ImageSection extends Component {
             }
         )
 
+
+        // add a submit button for pushing images
+
         // setting state for image append, change for firebase
     }
 
-    handleChangeInput = (event) => {
-        console.log('event', event.target.value);
+    handleSubmitImage = (event) => {
+        event.preventDefault();
+        this.setState({
+            isButtonDisabled: true
+        })
 
+        // console.log('event', event.target.value);
+    }
+
+    handleChangeInput = (event) => {
         this.setState({
             userInput: event.target.value
         })
@@ -65,12 +113,48 @@ class ImageSection extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        console.log('event', event.target);
+        // console.log('event', event.target);
+
+        // and if statement, if both radio button is selected and text area is filled, the data will be pushed then
+
+        // let radioBtn = document.getElementsByName('radio');
+
+        if (this.state.userInput !== '') {
+
+         if (document.querySelector("input[name=radio]:checked").length > 0) {
+            console.log('input has registed');
+            this.state.dbRefText.push(this.state.userInput);
+            this.state.dbRefImages.push(this.state.imageToAppend);
+
+        this.setState({
+            isButtonDisabled: true
+        })
+        }
+     } else {
+            console.log('this did not work')
+            // enter in error message function
+        } 
+
     }
 
     render() {
-        const textToAppend = this.state.userInput;
+        // const textToAppend = this.state.userInput;
 
+        const storedImages = this.state.imageArray.map( (image, i) => (
+            <StoredImages 
+            storedImg={image.imageUrl}
+            id={image.imageId}
+            key={i}
+            />
+        ))
+
+        const storedText = this.state.textArray.map((text, i) => (
+            <StorySection
+            storedText={text.storyText}
+            storedTextId={text.storyId}
+            key={i}
+            />
+        ))
         // appending text
 
         const imagesFinal = this.state.data.map( (response, i) => (
@@ -86,22 +170,28 @@ class ImageSection extends Component {
 
         return(
             <div className="App">
+                <form onSubmit={this.handleSubmit}>
                 <div className="imageSection">
                     <section>
                         <div className="imageContainer">
                             {imagesFinal}
                         </div>
+                        
                     </section>
+
                 </div>
+                    {/* <button type="submit">Select Image</button> */}
 
-                <form onSubmit={this.handleSubmit}>
                     <textarea onChange={this.handleChangeInput} name="" id=""></textarea>
-                    <button type="submit">Submit text here</button>
+                    <button disabled={this.isButtonDisabled} type="submit">Submit text here</button>
                 </form>
-
+                <div className="splitSection clearfix">
+                    <div className="imageSplit">{storedImages}</div>
+                    <div className="textSplit">{storedText}</div>
+                </div>
                 <StorySection
-                    appendImg={this.state.imageToAppend}
-                    textToBeAppended={textToAppend}
+                    // appendImg={this.state.imageToAppend}
+                    // textToBeAppended={textToAppend}
                 />
             </div>
         )
