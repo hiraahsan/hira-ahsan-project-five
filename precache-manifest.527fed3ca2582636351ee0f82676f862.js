9afcf4e78eb44d9acaa90aa7a05e8017d7daf96b
hira <hiraahsan94@gmail.com>
2019-12-02 14:23:55 -0500

    Updates

---
+++ precache-manifest.527fed3ca2582636351ee0f82676f862.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed08b69bc45362a77433664fab80092",
+    "url": "/hira-ahsan-project-five/index.html"
+  },
+  {
+    "revision": "ab0609a6bbc8ed0aa614",
+    "url": "/hira-ahsan-project-five/static/css/main.bcc3c498.chunk.css"
+  },
+  {
+    "revision": "b67e0e6f4215c2b98271",
+    "url": "/hira-ahsan-project-five/static/js/2.0c91c421.chunk.js"
+  },
+  {
+    "revision": "ab0609a6bbc8ed0aa614",
+    "url": "/hira-ahsan-project-five/static/js/main.f307786e.chunk.js"
+  },
+  {
+    "revision": "2870169038492edc8477",
+    "url": "/hira-ahsan-project-five/static/js/runtime-main.7895fda1.js"
+  }
+]);
